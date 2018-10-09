fdd3103fe3f3b832f33a1375e5d242faa7b22858
Hari Prasetyo <hari.prasetyo@phss.pertamina.com>
2018-10-09 08:33:21 +0700

    test

--- vuebnb-prototype/app.js
+++ vuebnb-prototype/app.js
@@ -17,6 +17,33 @@ var app = new Vue({
             'background-image': 'url(sample/header.jpg)'
         },
         amenities: sample.amenities,
-        prices: sample.prices
+        prices: sample.prices,
+        contracted: true,
+        textMore: "+ More",
+        textLess: "- Less",
+        modalOpen: false
+    },
+    watch: {
+        modalOpen: function() {
+            var className = 'modal-open';
+            if (this.modalOpen) {
+                document.body.classList.add(className);
+            } else {
+                document.body.classList.remove(className);
+            }
+        }
+    },
+    methods: {
+        escapeKeyListener: function(evt){
+            if (evt.keyCode === 27 && app.modalOpen) {
+                app.modalOpen = false;
+            }
+        }
+    },
+    created: function() {
+        document.addEventListener('keyup', this.escapeKeyListener);
+    },
+    destroyed: function() {
+        document.removeEventListener('keyup', this.escapeKeyListener);
     }
 });
